d47c221abcfec05745e97cedd8d704f879036b7f
mehedy3107 <mhador111@gmail.com>
2023-02-04 22:38:54 +0600

    navbar 2

--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -4,26 +4,26 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="w-full md:w-full lg:w-1/3 grid grid-cols-1 fixed bottom-0  lg:bottom-16 lg:left-1/3 ">
-      <div className="flex justify-around gap-2 bg-secondary px-3 py-5 lg:rounded-md ">
+    <nav className="w-full md:w-full lg:w-1/3 grid grid-cols-1 fixed bottom-0  lg:bottom-16 lg:left-1/3 bg-transparent backdrop-blur">
+      <div className="flex justify-around gap-2 bg-secondary px-3 py-5 lg:rounded-md">
         <NavLink to="/" className="p-3 hover:bg-neutral hover:rounded-md">
-          <ImHome3 className="text-2xl text-primary " />
+          <ImHome3 className="text-2xl text-black " />
         </NavLink>
 
         <NavLink to="/" className="p-3 hover:bg-neutral hover:rounded-md">
-          <ImHome3 className="text-2xl text-primary " />
+          <ImHome3 className="text-2xl text-black " />
         </NavLink>
 
         <NavLink to="/" className="p-3 hover:bg-neutral hover:rounded-md">
-          <ImHome3 className="text-2xl text-primary " />
+          <ImHome3 className="text-2xl text-black " />
         </NavLink>
 
         <NavLink to="/" className="p-3 hover:bg-neutral hover:rounded-md">
-          <ImHome3 className="text-2xl text-primary " />
+          <ImHome3 className="text-2xl text-black " />
         </NavLink>
 
         <NavLink to="/" className="p-3 hover:bg-neutral hover:rounded-md">
-          <ImHome3 className="text-2xl text-primary " />
+          <ImHome3 className="text-2xl text-black " />
         </NavLink>
       </div>
     </nav>
