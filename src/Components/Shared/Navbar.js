14ee644f47a12b6646546f55a2219c9c9c2ff15b
FarhanZizz <iamfarhanaziz@gmail.com>
2023-02-04 23:34:12 +0530

    navbar

--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -5,35 +5,59 @@ import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="custom-navbar w-full md:w-full lg:w-1/3  grid grid-cols-1 fixed bottom-0  lg:bottom-9 lg:left-1/3 bg-transparent ">
+    <nav className=" w-full md:w-full lg:w-1/3  grid grid-cols-1 fixed bottom-0  lg:bottom-9 lg:left-1/3 bg-transparent ">
       <div class="w-full max-w-md mx-auto">
-        <div class="px-7 bg-white shadow-2xl rounded-2xl mb-5">
-          <div className="flex justify-around gap-2 px-3 py-5 lg:rounded-md">
+        <div class=" bg-white shadow-2xl rounded-2xl">
+          <div className="flex justify-evenly p-4 lg:rounded-md">
             <NavLink
               className={({ isActive }) =>
                 isActive
-                  ? " text-2xl text-white bg-primary p-4 rounded-xl "
+                  ? "text-2xl text-white bg-primary p-3 rounded-xl "
                   : "text-2xl text-primary p-3 rounded-xl hover:bg-gray-600  hover:text-white"
               }
               to="/"
             >
               <ImHome3 />
             </NavLink>
-
-            <NavLink to="/" className="p-3 hover:bg-neutral hover:rounded-md">
-              <ImHome3 className="text-2xl text-primary " />
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "text-2xl text-white bg-primary p-3 rounded-xl "
+                  : "text-2xl text-primary p-3 rounded-xl hover:bg-gray-600  hover:text-white"
+              }
+              to="/"
+            >
+              <ImHome3 />
             </NavLink>
-
-            <NavLink to="/" className="p-3 hover:bg-neutral hover:rounded-md">
-              <ImHome3 className="text-2xl text-primary " />
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "text-2xl text-white bg-primary p-3 rounded-xl "
+                  : "text-2xl text-primary p-3 rounded-xl hover:bg-gray-600  hover:text-white"
+              }
+              to="/"
+            >
+              <ImHome3 />
             </NavLink>
-
-            <NavLink to="/" className="p-3 hover:bg-neutral hover:rounded-md">
-              <ImHome3 className="text-2xl text-primary " />
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "text-2xl text-white bg-primary p-3 rounded-xl "
+                  : "text-2xl text-primary p-3 rounded-xl hover:bg-gray-600  hover:text-white"
+              }
+              to="/"
+            >
+              <ImHome3 />
             </NavLink>
-
-            <NavLink to="/" className="p-3 hover:bg-neutral hover:rounded-md">
-              <ImHome3 className="text-2xl text-primary " />
+            <NavLink
+              className={({ isActive }) =>
+                isActive
+                  ? "text-2xl text-white bg-primary p-3 rounded-xl "
+                  : "text-2xl text-primary p-3 rounded-xl hover:bg-gray-600  hover:text-white"
+              }
+              to="/"
+            >
+              <ImHome3 />
             </NavLink>
           </div>
         </div>
