261f5d056163ce0d7ef94662c64c1700fe8f035f
FarhanZizz <iamfarhanaziz@gmail.com>
2023-02-28 23:26:06 +0530

    loading component made

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AboutUs from "./AboutUs";
 import MessengerCustomerChat from "react-messenger-customer-chat";
 import Banner from "./Banner";
@@ -6,9 +6,16 @@ import CreateYourCard from "./CreateYourCard";
 import Footer from "./Footer";
 import Information from "./Information";
 import ScrollToTop from "./ScrollToTop";
+import Loading from "../Loading/Loading";
+import { useContext } from "react";
+import { AuthContext } from "../../../AuthProvider/AuthProvider";
 
 // import ScrollToTop from "react-scroll-to-top";
 const Home = () => {
+  const { loading } = useContext(AuthContext);
+  if (loading) {
+    return <Loading></Loading>;
+  }
   return (
     <div className="mb-0">
       <Banner />
